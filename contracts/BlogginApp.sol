9f70839e0e8dc7b194832ee5fa8b534f94a1af9c
xadhrit <maharajrabel@gmail.com>
2023-02-25 04:04:16 +0530

    new tokens

--- contracts/BlogginApp.sol
+++ contracts/BlogginApp.sol
@@ -1,45 +1,100 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+import "./BloggyToken.sol";
 
-contract BloggingApp is ERC721, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract BloggingApp {
+    
+    struct BlogPost {
+        address author;
+        string title; 
+        string content;
+        uint256 timestamp;
+        uint256 views;
+        uint256 paymentReceived;
+        mapping(address => bool) likes;
+        mapping(address => bool) donationsReceived;
+    }
+    
+    address payable public owner;
+    BloggyToken public bloggyToken;
+    mapping(address => bool) public admins;
+    mapping(uint256 => BlogPost) public blogPosts;
+    uint256 public numBlogPosts = 0;
+    uint256 public minPayment = 0.01 ether;
 
-    Counters.Counter private _tokenIdCounter; 
-    uint256 public fees; 
+    event NewBlogPost(uint256 indexed postId, address indexed author, string title, string content );
+    event BlogPostLiked(uint256 indexed postId, address indexed liker);
+    event BlogPostDonated(uint256 indexed postId, address indexed donor, uint256 amount);
 
-    constructor ( string memory _name, string memory _symbol, uint256 fees_ )ERC721(_name, _symbol){
-        fees = fees_;
+    constructor(address _bloggyToken){
+        owner = payable(msg.sender);
+        bloggyToken = BloggyToken(_bloggyToken);
+        admins[msg.sender] = true;
+    }  
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
     }
 
-    function safeMint(address to, string memory uri) public payable {
-        require(msg.value >= fees, "Not enough MATIC");
-        payable (owner()).transfer(fees);
+    modifier onlyAdmin(){
+        require(admins[msg.sender], "Only admins can perform this action");
+        _;
+    } 
 
-        // mint nft
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-
-        // return oversupplied fees
-        uint256 contractBalance = address(this).balance; 
-
-        if (contractBalance > 0) {
-            payable(msg.sender).transfer(address(this).balance);
-        }
+    function addAdmin(address newAdmin) public onlyOwner {
+        admins[newAdmin] = true;
     }
 
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-
+    function removeAdmin(address admin) public onlyOwner {
+        delete admins[admin];
     }
 
-    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns(string memory) {
-        return super.tokenURI(tokenId);
+    function setMinPayment(uint256 _minPayment) public onlyAdmin {
+        minPayment = _minPayment;
+    }
+
+    function publishPost(string memory title, string memory content) public {
+        require(bytes(title).length > 0 && bytes(content).length > 0, "Title and content must not be empty");
+
+        blogPosts[numBlogPosts] = BlogPost({
+            author: msg.sender,
+            title: title,
+            content: content,
+            timestamp: block.timestamp,
+            views: 0,
+            paymentReceived: 0
+        });
+
+        emit NewBlogPost(numBlogPosts, msg.sender, title, content); 
+        numBlogPosts++;
+    } 
+
+    function likePost(uint256 postId) public {
+        require(postId < numBlogPosts, "Post does not exist");
+        require(!blogPosts[postId].likes[msg.sender],"POst already liked by this address");
+
+        blogPosts[postId].likes[msg.sender] = true;
+        bloggyToken.awardTokens(postId);
+        emit BlogPostLiked(postId, msg.sender);
+    }
+
+    function donate(uint256 postId) public payable {
+        require(postId < numBlogPosts,"Post does not exist");
+        require(msg.value >= minPayment, "Donation amount must be at least 0.01");
+        require(!blogPosts[postId].donationsReceived[msg.sender], "Donation already received from this address");
+        
+        blogPosts[postId].donationsReceived[msg.sender] = true; 
+        blogPosts[postId].paymentReceived += msg.value; 
+        emit BlogPostDonated(postId, msg.sender,msg.value);
+    }
+
+    function withdraw() public onlyOnwer {
+        owner.transfer(address(this).balance);
+    }
+
+    functino widthdrawTokens() public onlyOwner {
+        bloggyToken.transfer(owner, bloggyToken.balanceOf(address(this)));
     }
 }
---
+++ contracts/BloggyToken.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BloggyToken is ERC20 {
+    address owner;
+    uint256 public viewThreshold;
+    mapping(address => mapping(uint256 => bool)) public hasViewed;
+
+    event TokensAwarded(address indexed blogger, uint256 amount);
+    event ViewRecorded(address indexed viewer, uint256 indexed postId);
+
+    constructor(uint256 _initialSupply, uint256 _viewThreshold) ERC20("Bloggy Token", "BLOG"){
+        owner = msg.sender;
+        _mint(msg.sender, _initialSupply * 10 ** decimals());
+        viewThreshold = _viewThreshold;
+    }
+
+    function awardTokens(uint256 postId) public {
+        require(hasViewed[msg.sender][postId],"Blogger has not received any token award");
+        require(!hasReachedViewThreshold(msg.sender, postId),"Blogger has already received tokens for this blog post");
+
+        _mint(msg.sender, 1 * 10 ** decimals());
+        emit TokensAwarded(msg.sender, 1);
+
+    }
+
+    function hasReachedViewThreshold(address blogger, uint256 postId) public view returns (bool) {
+        return hasViewed[blogger][postId] && (balanceOf(blogger) >= (views(blogger, postId) / viewThreshold));
+    }
+
+    function setViewThreshold(uint256 _viewThreshold) public {
+        require(msg.sender == owner, "Only the contract owner can set the view threshold");
+        viewThreshold = _viewThreshold;
+    }
+
+    function views(address blogger, uint256 postId) public view returns (uint256) {
+        if (hasViewed[blogger][postId]){
+            return balanceOf(blogger);
+        } 
+        return 0;
+    }
+
+    function recordView(uint256 postId) public {
+        require(!hasViewed[msg.sender][postId], "View already recorded for this address and post");
+        hasViewed[msg.sender][postId] = true;
+        _mint(owner , 1 * 10 ** decimals());
+        emit ViewRecorded(msg.sender, postId);
+    }
+
+}
